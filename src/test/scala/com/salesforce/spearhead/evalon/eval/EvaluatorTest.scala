64b77c0763dfb5dcfc2a21ebf82a9ae334a3e1f1
Yuxi Zhang <yuxi.zhang@salesforce.com>
2026-09-12 11:54:22 -0700

    update tests

--- src/test/scala/com/salesforce/spearhead/evalon/eval/EvaluatorTest.scala
+++ src/test/scala/com/salesforce/spearhead/evalon/eval/EvaluatorTest.scala
@@ -46,8 +46,22 @@ class EvaluatorTest extends AnyFunSuite:
       participants = Map.empty,
       conversations = Nil,
       evalCriteria = List(
-        EvalCriterion("rebooked the flight", 2.0),
-        EvalCriterion("confirmed with the user", 1.0)
+        EvalCriterion(
+          name = "rebooked",
+          description = "rebooked the flight",
+          criterionType = CriterionType.Binary,
+          requireToolCall = true,
+          sourceType = CriterionSourceType.OpProcedure,
+          weight = 2.0,
+        ),
+        EvalCriterion(
+          name = "confirmed",
+          description = "confirmed with the user",
+          criterionType = CriterionType.Binary,
+          requireToolCall = false,
+          sourceType = CriterionSourceType.History,
+          weight = 1.0,
+        ),
       )
     )
 
